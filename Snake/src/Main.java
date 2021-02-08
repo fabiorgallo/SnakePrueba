fa8deee5c8abe61350ebdeaedbb0e76fc4edf183
FabioGalloGlo <fabio.gallo@globant.com>
2021-02-08 15:51:30 -0300

    new comments

--- Snake/src/Main.java
+++ Snake/src/Main.java
@@ -3,10 +3,9 @@
  * @author
  */
 public class Main {
-    /**
-     * 
-     */
     public static void main(String[] args) {
         new Snake();
+        // atom comment
+        new Snake() ;
     }
 }
